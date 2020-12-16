7c1b795f61c9c0b4aa413e6dcb3780bf731c43ed
chylomicronman <bmccann36@gmail.com>
2020-12-16 17:16:37 -0500

    added code to remove styles from div

--- test-html-parse-logic/src/removeStyle/removeInlineStyle.js
+++ test-html-parse-logic/src/removeStyle/removeInlineStyle.js
@@ -3,12 +3,31 @@ const cheerio = require('cheerio');
 class RemoveInlineStyle {
   constructor() {}
 
+  /**
+   * select only the content of the <td> tag with id EMAIL_CONTAINER and returns its html content
+   * @param {string} nlHtmlString
+   * @return {string} contents of EMAIL_CONTAINER td tag
+   */
   selectEmailContainerContent(nlHtmlString) {
     // load data into cheerio parser
     const $ = cheerio.load(nlHtmlString);
     // select only the content container with the newsletter content
     const emailContent = $('td[id="EMAIL_CONTAINER"]').html();
     return emailContent;
+  }
+
+  /**
+   * 
+   * @param {string} htmlEmailContent 
+   * @param {string} html with style removed
+   */
+  removeAllStyles(htmlEmailContent) {
+    const $ = cheerio.load(htmlEmailContent);
+    // remove div styling
+    $('div').each(function (i, el) {
+      $(el).attr('style', null);
+    });
+    return $.html();
   }
 }
 
--- test-html-parse-logic/src/removeStyle/removeInlineStyle.spec.js
+++ test-html-parse-logic/src/removeStyle/removeInlineStyle.spec.js
@@ -2,10 +2,18 @@ const RemoveInlineStyle = require('./removeInlineStyle');
 const fs = require('fs');
 const path = require('path');
 const { html, renderToString } = require('@popeindustries/lit-html-server');
+const cheerio = require('cheerio');
 
 let target;
 
-const sampleNlData = fs.readFileSync(path.join(__dirname, 'morning_newsletter.html')).toString();
+
+//? FOR TESTING
+const sampleNlData = fs
+  .readFileSync(path.join(__dirname, 'morning_newsletter.html'))
+  .toString();
+const contentOnlySample = fs
+  .readFileSync(path.join(__dirname, 'email_content_only.html'))
+  .toString();
 
 describe('RemoveInlineStyle', () => {
   beforeEach(() => {
@@ -13,9 +21,24 @@ describe('RemoveInlineStyle', () => {
   });
   it('selects only the html within <td> tag where "id=EMAIL_CONTAINER"', async () => {
     const htmlSample = await getMockNlHtmlDoc();
-    console.log(htmlSample);
-    // const reformatted = target.selectEmailContainerContent(sampleNlData);
-    // fs.writeFileSync(path.join(__dirname, 'htmlResult.html'), reformatted);
+    const selectedContent = target.selectEmailContainerContent(htmlSample);
+    const $ = cheerio.load(selectedContent); // load the output
+    const divContents = $('div').text(); // get contents of div
+    expect(divContents).toBe('TEST DIV WITH CONTENT');
+    //? to write to file when we want
+    // fs.writeFileSync(path.join(__dirname, 'email_content_only.html'), selectedContent);
+  });
+
+  it('removes ALL style from all the divs', async () => {
+    const emailContentHtmlSample = await getMockEmailContent();
+    const unstyledResult = target.removeAllStyles(emailContentHtmlSample);
+    // console.log(unstyledResult);
+    const $ = cheerio.load(unstyledResult);
+    $('div').each((i, el) => {
+      const divStyle = $(el).attr('style');
+      expect(divStyle).toBe(undefined);
+    });
+    // fs.writeFileSync(path.join(__dirname, 'div_style_removed.html'), unstyledResult);
   });
 });
 
@@ -45,5 +68,21 @@ async function getMockNlHtmlDoc() {
       </body>
     </html>
   `;
-  return renderToString(myHtml);
+  const renderedHtml = await renderToString(myHtml);
+  return renderedHtml.replace(/(\r\n|\n|\r)/gm, '');
+}
+async function getMockEmailContent() {
+  const myHtml = html`
+    <div style="margin: 0 auto; max-width: 600px; width: 100%">
+      <p>div01</p>
+    </div>
+    <div style="margin: 0 auto; max-width: 600px; width: 100%">
+      <p>div02</p>
+    </div>
+    <div style="margin: 0 auto; max-width: 600px; width: 100%">
+      <p>div03</p>
+    </div>
+  `;
+  const renderedHtml = await renderToString(myHtml);
+  return renderedHtml.replace(/(\r\n|\n|\r)/gm, '');
 }
